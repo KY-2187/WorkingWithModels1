38cedf152b32bad5aba747b967f2236d7104d5ed
KY-2187 <58202175+KY-2187@users.noreply.github.com>
2020-06-16 12:40:52 +0800

    edited

--- Working With Models 1/functional_test.py
+++ Working With Models 1/functional_test.py
@@ -1,8 +1,11 @@
 import unittest
+import time
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-class NewVisitorTest(unittest, TestCase):
+
+class NewVisitorTest(unittest.TestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -15,19 +18,31 @@ class NewVisitorTest(unittest, TestCase):
 		self.assertIn('To-Do', self.browser.title)
 
 	def test_can_create_new_list(self):
-		self.browser.get('http://localhost:8000/tasks')
+		self.browser.get('http://localhost:8000')
 		input = self.browser.find_element_by_id('id_new_item')
-		self.assertEqual(
-			input.get_attribute('placeholder'),
-			'Enter a to-do item'
-		)
-		self.assertIn(
-			browser.getCurrentUrl(),
-			'http://localhost:8000/task/1'
-		)
+
+		task_list = self.browser.find_element_by_id('task_list')
+		task_listitems = task_list.find_elements_by_tag_name('li')
+
+		input.send_keys('Finish CSCI40 Project')
+		input.send_keys(Keys.ENTER)
+		time.sleep(1)
+
 		self.assertIn(
 			'Finish CSCI40 Project',
-			self.browser.find_element_by_id('task_name').text
+			[item.text for item in task_listitems]
 		)
+#		self.assertEqual(
+#			input.get_attribute('placeholder'),
+#			'Enter a to-do item'
+#		)
+#		self.assertIn(
+#			browser.getCurrentUrl(),
+#			'http://localhost:8000/task/1'
+#		)
+#		self.assertIn(
+#			'Finish CSCI40 Project',
+#			self.browser.find_element_by_id('task_name').text
+#		)
 if __name__ == '__main__':
 	unittest.main(warnings='ignore')
